832f2ed423bc70730b15130958e4bbc141c14858
Phillip Baker <16352712+phillipbaker@users.noreply.github.com>
2024-06-05 09:30:04 -0400

    Add stylsheet

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,28 +3,34 @@ function searchDestination() {
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
-    fetch('health_analysis.json')
-      .then(response => response.json())
-      .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            const condition = data.destinations.find(item => item.name.toLowerCase() === input);
+            console.log("Input:", input);
+            console.log("First Destination:", data.destinations[0]);
 
-        if (condition) {
-          const symptoms = condition.symptoms.join(', ');
-          const prevention = condition.prevention.join(', ');
-          const treatment = condition.treatment;
+            if (destination) {
+                resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+                resultDiv.innerHTML += `<h2>${destination.name}</h2>`;
 
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+                resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+                resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+                resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+            } else {
+                resultDiv.innerHTML = 'Destination not found.';
+            }
+        })
+        
+        .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data.';
+        });
+}
 
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
-        } else {
-          resultDiv.innerHTML = 'Condition not found.';
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        resultDiv.innerHTML = 'An error occurred while fetching data.';
-      });
-  }
+function thankyou() {
+    alert('Thank you for contacting us!');
+}
+
+searchBtn.addEventListener('click', searchDestination);
+contactBtn.addEventListener('click', thankyou);
